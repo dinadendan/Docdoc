5526bf4ae7a214c372f7b9db8e02703079743296
dinadendan <dina.dendan222@gmail.com>
2024-09-04 04:28:06 +0300

    add doctors blue container

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:docdoc_app/features/home/ui/doctors_blue_container.dart';
 import 'package:docdoc_app/features/home/ui/home_top_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,12 @@ class HomeScreen extends StatelessWidget{
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          margin: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 28.0,),
+          margin: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 28.0,),
           child: const Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-             HomeTopBar(),
+              HomeTopBar(),
+              DoctorsBlueContainer(),
             ],
           ),
         ),
---
+++ lib/features/home/ui/doctors_blue_container.dart
@@ -0,0 +1,69 @@
+import 'package:docdoc_app/core/helper/spacing.dart';
+import 'package:docdoc_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DoctorsBlueContainer extends StatelessWidget{
+  const DoctorsBlueContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 195.h,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 165.h,
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.w,
+              vertical: 16.h,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24.r),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/home_blue_pattern.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Book and\nschedule with\nnearest doctor',
+                  style: TextStyles.font18WhiteMedium,
+                  textAlign: TextAlign.start,
+                ),
+                verticalSpace(16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(48.r),
+                      ),
+                    ),
+                    child: Text(
+                      'Find Nearby',
+                      style: TextStyles.font12BlueRegular,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 8.w,
+            top: 0,
+            child: Image.asset(
+              'assets/images/doc.png',
+              height: 200.h,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
